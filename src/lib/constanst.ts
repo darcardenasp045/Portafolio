16d5c6dc64e2b903db74454019d0e48ddac30c92
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2024-01-29 18:48:37 -0500

    We finish the front of our portfolio

--- src/lib/constanst.ts
+++ src/lib/constanst.ts
@@ -1,5 +1,6 @@
 export const LINKS = {
     github: "https://github.com/darcardenasp045",
-    linkedin: "https://www.linkedin.com/darcardenasp045",
-    
+    linkedin: "https://www.linkedin.com/in/daniel-cardenas-3725381b0/",
+    twitter: "https://twitter.com/darmandocardena",
+  
   };
